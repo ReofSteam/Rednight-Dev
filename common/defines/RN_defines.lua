6583287735e74b088406d2063525678aa8735df8
ReofSteam <41215145+ReofSteam@users.noreply.github.com>
2022-01-24 15:13:16 -0500

    spain ideologies  and other things

--- common/defines/RN_defines.lua
+++ common/defines/RN_defines.lua
@@ -2,7 +2,7 @@
 -- Commented out items are under re-evaluation
 
 -- Links
-NDefines.NWiki.BASE_URL = "https://kaiserreich.fandom.com/wiki/The_Kaiserreich_Wiki"
+NDefines.NWiki.BASE_URL = "https://rednight.fandom.com/wiki/Red_Night:_A_Nightmare_in_Europe"
 -- Vanilla is "http://www.hoi4wiki.com/"
 
 -- Game
